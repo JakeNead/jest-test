2be4423dba1fe2c6892d1af7783b96d0abcc40df
Jake Nead <jakenead@gmail.com>
2023-10-31 11:56:56 -0700

    All analyzeArray tests pass

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,5 +1,12 @@
 function analyzeArray(arr) {
   if (arr === undefined) return "You need to pass an array as an argument!";
-}
+  arr.sort((a, b) => a - b);
+  const average = arr.reduce((a, b) => a + b) / arr.length;
+  const min = arr[0];
+  const max = arr[-1];
+  const length = arr.length;
 
+  return { average, min, max, length };
+}
+//
 module.exports = analyzeArray;
--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -3,3 +3,7 @@ const analyzeArray = require("../src/analyzeArray");
 test("No argument given", () => {
   expect(analyzeArray()).toBe("You need to pass an array as an argument!");
 });
+
+test("Returns average", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
